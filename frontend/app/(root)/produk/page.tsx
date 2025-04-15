b7d952047200568a1d425cc7db62e9a4735fd187
ReinDaze <gustavitoputra@gmail.com>
2025-04-16 03:11:21 +0700

    produk page statis

---
+++ frontend/app/(root)/produk/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import DetailProduk from "../components/produkcomp/detailproduk";
+
+export default function Home() {
+  return (
+    <>
+        <DetailProduk/>
+    </>
+  );
+}
