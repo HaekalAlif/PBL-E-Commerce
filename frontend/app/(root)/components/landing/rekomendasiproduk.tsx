f9cb430caef245a85616d650c1216f5a2f0b50d0
ReinDaze <gustavitoputra@gmail.com>
2025-04-16 15:35:36 +0700

    fix responsif rekomendasi produk

--- frontend/app/(root)/components/landing/rekomendasiproduk.tsx
+++ frontend/app/(root)/components/landing/rekomendasiproduk.tsx
@@ -60,7 +60,7 @@ export default function Rekomendasi() {
           </div>
 
           {/* Product Section */}
-          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6 flex-1">
+          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6 flex-1">
             {products.map((product, index) => (
               <div
                 key={index}
